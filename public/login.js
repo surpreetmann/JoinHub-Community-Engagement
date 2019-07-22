1c010955193279fee5c1a9242e630f967619add2
surpreetmann <surpreetmann35@gmail.com>
2019-07-22 23:43:35 +0530

    Initial commit

---
+++ public/login.js
@@ -0,0 +1,27 @@
+var username = document.getElementById("username");
+var password = document.getElementById("password");
+var log = document.getElementById("logbtn");
+var git=document.getElementById("githubbtn");
+
+log.addEventListener("click", function() {
+  if (username.value != "" && password.value != "") {
+    var request = new XMLHttpRequest();
+    request.addEventListener("load", function() {
+      var data = JSON.parse(request.responseText);
+      console.log(data);
+      if (data.length > 0) {
+        alert("sucessful");
+      } else {
+        alert("wrong username or password");
+        password.value = "";
+      }
+    });
+    request.open("POST", "/log");
+    request.setRequestHeader("Content-Type", "application/json");
+    request.send(
+      JSON.stringify({ username: username.value, password: password.value });
+    );
+  } else {
+    alert("All fields are required");
+  }
+});
